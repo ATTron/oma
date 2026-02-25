21288f060ab5815aa463cdb33511d8a563da0729
Anthony Templeton <twantempleton11@gmail.com>
2026-02-25 16:28:31 -0500

    feat/docs

--- build.zig
+++ build.zig
@@ -22,6 +22,24 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&b.addRunArtifact(mod_tests).step);
+
+    const lib = b.addObject(.{
+        .name = "oma",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/root.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+
+    const docs_step = b.step("docs", "Generate documentation");
+    docs_step.dependOn(&install_docs.step);
 }
 
 /// Compile `source` once per CPU level and link all variants into `compile`.
